9678b353f813bae2ed6a77fc88aab2b98b866d3d
Doggala David <pauldavid551@gmail.com>
2025-05-22 15:25:51 +0530

    changes made

--- playwright.config.js
+++ playwright.config.js
@@ -24,13 +24,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ['html'],
+  reporter: [ ['html' , { outputFolder: 'playwright-report', open: 'never' }],
             //['list' , {printSteps: true}],
             //['line'],
             //['dot'],
               // ['json' , {outputFile:'results.json'}],
               // ['junit' , {outputFile:'results.xml'}],
-              // ['allure-playwright', {outputFolder:'allure-results'}]
+              ['allure-playwright', {outputFolder:'allure-results'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
